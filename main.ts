38748f4a782bfd1d7acdf34609401a08cecbb185
Eoin H <eoin.hurrell@gmail.com>
2025-04-15 21:40:37 +0100

    chore: Make command identifiers more unique

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ export default class InboxTimelinePlugin extends Plugin {
 
 		// Register the Add to Inbox command
 		this.addCommand({
-			id: "add-to-inbox",
+			id: "inboxer-add-to-inbox",
 			name: "Add to Inbox",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				this.addToInbox(editor);
@@ -35,7 +35,7 @@ export default class InboxTimelinePlugin extends Plugin {
 
 		// Register the Add to Timeline command
 		this.addCommand({
-			id: "add-to-timeline",
+			id: "inboxer-add-to-timeline",
 			name: "Add to Timeline",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				this.addToTimeline(editor);
